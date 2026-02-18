d70a314df5c9276487d4d6a22e7a7a8baee01212
Sanika Aeer <sanikaaeerstudentid20@gmail.com>
2026-02-19 03:23:20 +0530

    Update settings for local development and other modifications

--- toppers_project/settings.py
+++ toppers_project/settings.py
@@ -12,9 +12,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-toppers-coaching-class-management-system-2024'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = ['toppers-coaching-class-management-system.onrender.com']
+ALLOWED_HOSTS = []
 
 # Application definition
 INSTALLED_APPS = [
