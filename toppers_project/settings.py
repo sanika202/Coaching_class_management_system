57c47fd1de9ee2e048b3f2dd2ea1b5aea0b9f325
Sanika Aeer <sanikaaeerstudentid20@gmail.com>
2026-02-18 22:43:47 +0530

    production settings update

--- toppers_project/settings.py
+++ toppers_project/settings.py
@@ -12,7 +12,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-toppers-coaching-class-management-system-2024'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
