86350a8ac4c031f3b754a43ef5884e373f61b02c
Sanika Aeer <sanikaaeerstudentid20@gmail.com>
2026-02-18 23:58:17 +0530

    Prepared project for deployement

--- toppers_project/settings.py
+++ toppers_project/settings.py
@@ -14,7 +14,7 @@ SECRET_KEY = 'django-insecure-toppers-coaching-class-management-system-2024'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['toppers-coaching-class-management-system.onrender.com']
 
 # Application definition
 INSTALLED_APPS = [
