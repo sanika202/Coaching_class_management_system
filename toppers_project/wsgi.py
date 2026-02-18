0076fc3bbb26e15e9973f2cb540afc5e7013a486
Sanika Aeer <sanikaaeerstudentid20@gmail.com>
2026-02-19 00:11:31 +0530

    Prepared project for deployement

--- toppers_project/wsgi.py
+++ toppers_project/wsgi.py
@@ -9,3 +9,18 @@ from django.core.wsgi import get_wsgi_application
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'toppers_project.settings')
 
 application = get_wsgi_application()
+
+import os
+from django.contrib.auth import get_user_model
+
+def create_superuser():
+    User = get_user_model()
+    username = os.environ.get("DJANGO_SUPERUSER_USERNAME")
+    email = os.environ.get("DJANGO_SUPERUSER_EMAIL")
+    password = os.environ.get("DJANGO_SUPERUSER_PASSWORD")
+
+    if username and email and password:
+        if not User.objects.filter(username=username).exists():
+            User.objects.create_superuser(username, email, password)
+
+create_superuser()
